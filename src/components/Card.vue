074420d3b0fcfaf850cad96c5dff35e788980f06
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2019-05-27 15:53:51 -0300

    nova implementação com Vue

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="border m-6 rounded-xs mx-auto max-w-lg shadow-lg rounded-lg overflow-hidden bg-gray-200">
+    <div class="sm:flex sm:items-center px-3 py-3">
+      <img
+        class="block h-48 rounded mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+        :src="imageUrl"
+      >
+      <div class="text-center sm:text-left sm:flex-grow">
+        <div class="mb-4">
+          <p class="text-xl leading-tight">
+            <slot name="title"></slot>
+          </p>
+          <p class="text-sm leading-tight text-grey-dark">
+            <slot></slot>
+          </p>
+        </div>
+        <div class="flex flex-wrap justify-center mb-1">
+          <slot name="actions"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import logo from "@/assets/logo.png";
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      default: logo
+    }
+  }
+};
+</script>
---
+++ src/components/Pill.vue
@@ -0,0 +1,47 @@
+<template>
+  <div @click="$emit('click')" :class="styleClasses">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    textColor: {
+      type: String,
+      default: "white"
+    },
+    color: {
+      type: String,
+      default: "blue"
+    }
+  },
+  data() {
+    return {
+      styleClasses: [
+        "transition-fade",
+        "icon-pointer",
+        "icon-padding",
+        "mr-1",
+        "border-b-1",
+        "rounded",
+        "px-2",
+        "bg-transparent",
+        `hover:bg-${this.color}-400`,
+        "hover:text-gray-100",
+        "text-gray-600",
+        `border-${this.color}-700`,
+        `hover:border-${this.color}-500`
+      ]
+    };
+  }
+};
+</script>
+<style>
+.icon-pointer {
+  cursor: pointer;
+}
+.icon-padding {
+  padding-top: 0.01em;
+  padding-bottom: 0.2em;
+}
+</style>
